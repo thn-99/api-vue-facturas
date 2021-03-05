868b4f1197731c5d7df79f60d7bcd443930cebfa
Tahseen <thn.ahmd99@gmail.com>
2021-03-05 14:27:34 +0100

    nose

--- api/index.php
+++ api/index.php
@@ -15,10 +15,10 @@ $uri = basename($_SERVER['REQUEST_URI']);
 $entity = getEntityManager();
 $clienteRepository = new ClienteRepository();
 
-  echo "method: ".$method;
-  echo "uri: ".$uri;
- var_dump($_GET);
- var_dump($_POST);
+//echo "method: ".$method;
+//echo "uri: ".$uri;
+//var_dump($_GET);
+//var_dump($_POST);
 
 
 if (strpos($uri, "?")) {
